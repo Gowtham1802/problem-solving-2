d85d9eccb34c6c5d3339ac586a72742acf243fc3
Gowtham1802 <gowthama7575@gmail.com>
2023-07-12 11:19:51 +0530

    add

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict'
+const num = 5;
+console.log(num);
+
+// 1.Write a JavaScript function to check if a given number is even or odd.
+
+const myNumber = prompt("");
+if(!isNaN(Number(myNumber))){
+  if(myNumber % 2 == 0 ){
+  console.log("the number is even" )
+  }else{
+    console.log("the number is odd")
+  }
+}
+else{
+  console.log(`not a string`);
+}
+
+
+2.Write a JavaScript program to calculate the factorial of a number.
+
